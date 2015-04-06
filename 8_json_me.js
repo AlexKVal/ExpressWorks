4c9292289d2c50c5ef8f6ad489ab3509e1f8da6b
AlexKVal <alexkval@gmail.com>
2015-04-06 13:29:51 +0300

    8_json_me.js

--- 8_json_me.js
+++ 8_json_me.js
@@ -6,7 +6,17 @@ var fs = require('fs');
 var app = express();
 
 app.get('/books', function (req, res) {
-  res.json(JSON.parse(fs.readFileSync(process.argv[3])));
+  fs.readFile(process.argv[3], function (err, data) {
+    if (err) {
+      return res.send(500);
+    }
+
+    try {
+      res.send(JSON.parse(data));
+    } catch (e) {
+      res.send(500);
+    }
+  });
 });
 
 app.listen(process.argv[2]);
